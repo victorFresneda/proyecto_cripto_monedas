9e0f835d2bbe66fd0ea5300c41b27dc6d1ab2152
victorFresneda <vafresnedah@hotmail.com>
2021-08-07 23:28:23 -0500

    Se crea la funcion leerValor

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
-const criptoMonedasSelect = document.querySelector('#criptomenedas');
+const criptoMonedasSelect = document.querySelector('#criptomonedas');
+const monedasSelect = document.querySelector('#moneda');
+const formulario = document.querySelector=('#formulario');
+
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+}
 
 
 //Crear un promise 
@@ -10,7 +18,12 @@ const obtenerCriptomonedas = criptoMonedas => new Promise(resolve => {
 document.addEventListener('DOMContentLoaded', () => {
 
     consultarCriptoMonedas();
-}) 
+
+    formulario.addEventListener('submit', submitFomulario);
+
+    criptoMonedasSelect.addEventListener('change', leerValor);
+    monedasSelect.addEventListener('change', leerValor);
+}); 
 
 
 function consultarCriptoMonedas (){
@@ -29,13 +42,41 @@ function selectCriptomonedas(criptoMonedas){
 
     criptoMonedas.forEach( cripto => {
 
-        console.log(cripto);
+
+        const {FullName, Name } = cripto.CoinInfo;
+
+        const option = document.createElement('option');
+        option.value = Name;
+        option.textContent = FullName;
+        criptoMonedasSelect.appendChild(option);
+
         
-    });
+        });
+}
 
 
+function leerValor(e){
+    objBusqueda[e.target.name] = e.target.value;
+    
+}
+
+
+function submitFomulario(e){
+    e.preventDefault();
+
+    //validar
+
+    const {moneda, criptomoneda} = objBusqueda;
+
+    if( moneda === '' || criptomoneda === ''){
+        mostrarAlerta('Ambos campos son obliga')
+        return;
+    }
 
 
 }
 
+function mostrarAlerta(msg){
+    console.log(msg)
+}
      
